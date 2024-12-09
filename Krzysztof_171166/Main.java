6513c1edfa93db581d0a15294a90a4254df14240
grzysiek <grzysiek.xd@gmail.com>
2024-12-09 22:32:23 +0100

    Dodanie kodów źródłowych

---
+++ Krzysztof_171166/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a=125;
+        System.out.println(a);
+        int wynik=a/2;
+        System.out.println(wynik);
+        System.out.println(a++);
+        System.out.println(++a);
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b1 || b2);
+        System.out.println(b1 && b2);
+    }
+}
