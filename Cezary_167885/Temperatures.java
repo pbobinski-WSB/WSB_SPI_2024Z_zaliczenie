69124d2fb99c18c3045c302b10371eac51d83dab
Cezixx <cmarkiewicz60@gmail.com>
2024-12-11 19:07:53 +0100

    Zmiana nazwy zmiennej

--- Cezary_167885/Temperatures.java
+++ Cezary_167885/Temperatures.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Temperatures {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double celsiusDegrees,fahrenheitDegrees;
+        double degreesCelsius,fahrenheitDegrees;
 
         System.out.println("Insert temperature in Celsius:");
-        celsiusDegrees = input.nextDouble();
+        degreesCelsius = input.nextDouble();
         input.close();
 
-        fahrenheitDegrees = 1.8 * celsiusDegrees + 32.0;
+        fahrenheitDegrees = 1.8 * degreesCelsius + 32.0;
 
         System.out.println("After conversion Celsius degress on Fahrenheit degrees you get:" + " " + fahrenheitDegrees + " F");
 
