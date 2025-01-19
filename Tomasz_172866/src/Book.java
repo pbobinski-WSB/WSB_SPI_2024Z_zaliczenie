3e78b9d8244d2df2f3532f0863116185637a4fab
tomaz <wwx28183@student.warszawa.merito.pl>
2025-01-19 13:41:08 +0100

    Dodanie klas Reader i Book

---
+++ Tomasz_172866/src/Book.java
@@ -0,0 +1,17 @@
+
+
+public class Book {
+    String title;
+    String author;
+    int pages;
+    String genre;
+
+    public Book(String title, String author, int pages, String genre) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.genre = genre;
+    }
+
+
+}
--- Tomasz_172866/src/Main.java
+++ Tomasz_172866/src/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome in library");
+        Book SimpleCpp = new Book("Simple C", "Stroustup", 1234, "programing");
+        Book Java9 = new Book("Java 9", "Horstmann", 500, "programing");
+        Reader  reader = new Reader("Wieslaw", "Przykladowy");
+        reader.checkout(SimpleCpp);
+        reader.checkout(Java9);
+        reader.hireBooks.stream().forEach(s -> System.out.println(s));
 
     }
 }
