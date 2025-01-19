1db823efa6d1f18c259d046c35bd0343d8b70148
mtskzx <matiis2004@gmail.com>
2025-01-19 19:56:46 +0100

    Zmieniono "swiecie" na imie, ktore poda uzytkownik

---
+++ Mateusz_176155/witajimie.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string imie;
+
+    cout << "Podaj swoje imie: ";
+    cin >> imie;
+
+    cout << "Witaj, " << imie << "!" << endl;
+    return 0;
+}
